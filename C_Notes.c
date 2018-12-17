acfc0ffc375cf968c50f7167ec3e4185eb219c35
home <amirsaleem1990@hotmail.com>
2018-12-17 21:05:34 +0500

    updated

--- C_Notes.c
+++ C_Notes.c
@@ -218,7 +218,7 @@ main()
 	printf("%f", avg);
 }
 ----------------------------------------------
- 
+// Two dimentional Arrays 
 ----------------------------------------------
 
 ----------------------------------------------
@@ -358,10 +358,221 @@ main()
 ----------------------------------------------
 
 ----------------------------------------------
+// address operator
 
+// & is address operator
+// it is an unary operator
+// operand must be the variable name
+// & operator gives address number of variable
+// & is also known as referecing operator
+printf("%d", &variable_name);
+// the address will lay between 0 and 65535, if we use "%d" to print this value we can get between -32768 to 32767, by using "%u" we get values between 0 and 65535
 ----------------------------------------------
+// indirection operator
 
+// * is indirection operator
+// it is also known as dereferensing operator
+// it is an unarty operator
+// it takes address as an argument
+// * returns the content / container whose address is its argument
+printf("%d", *&x);
+// the address will lay between 0 and 65535, if we use "%d" to print this value we can get between -32768 to 32767, by using "%u" we get values between 0 and 65535
 ----------------------------------------------
+point:
+int x=5;
+printf("%d %u", &x) // let us assume it is return 2048
+&x = 7;
+// Error, we can't assing <7> to 2048, becouse 2048 is constant, and we could'nt assign value to constant 
+----------------------------------------------
+// Pointers
+// variable contain address of another variable address
+// Pointers always consumes 2 bytes in memory
+// syntax:
+int x=5, *j;
+j = &x;
+// here we assing address of <x> in <j>, so <j> is pointer contain <x> address, if <&x> == 2000 <j> cointain this <int> value
+
+// define pointer for <int>
+int *j;
+// define pointer for <float>
+float *j;
+// define pointer for <char>
+char *j;
+// Note: pointer and his variable must be same type, so we can't assig int variable address to float pointer, yce bohot aham baat h k agar mujhy int variable ka pointer banana h to me us pointer ko assign karty waqt us ki value int hi define karun ga.
+--------------------------------------------
+// Note:
+// <*> and <&> cancel each other so <*&j> == <j>
+// whenever you see formal arguments are pointer variables the fuction call contain addresses not a variables
+----------------------------------------------
+// Extended concept of Pointers
+// we can assign pointer to another pointer, but jis level ka pointer ho ga us ko define karty waqt us sy pehly utni hi histaric lagayen gy, eg:
+
+main()
+{
+	int x, *p, **q, ***r;
+	x = 5;
+	p = &x; // Level1 pointer, becouse it have one *, <p> me srif us variable ka address aa sakta h jo khud pointer na ho, not else.
+	q = &p; // Level2 pointer, becouse it have Two *, <q> srif pointer of pointer ban sakta h, not else.
+	r = &q; // Level3 pointer, becouse it have three *, <r> srif pointer of pointer of pointer ban sakta h, not else.  
+
+	// we need to assign value in x<int> by using <q><level 2> pointer;
+	**q = 7; // here first we evelvate <*q> which is <p> and than <*p> which is <x>.
+
+	// we need to assign value in x<int> by using <r><level 3> pointer;
+	***r = 7; // here first we evelvate <*r> which is <q>, than <*q> which is <*p>, than <*p> which is <x>
+
+}
+
+----------------------------------------------
+// Pointer's Arithmetic
+// 1- We cannot add, multiply or devide two addresses.
+// 2- we cannot multiply an integer to an adress and similarly we cannot devide an address with an interget value.
+// 3- we can add or subtract to/from an adress
+
+// assume <&x> is 1000 and x in integer, and <y> is pointer of <x>, if we need add number to this address, and type:
+y+1 // we get 1002, becouse the next block of <x> starts with 1002, since <x> is an int, and int consume 2 bytes, if <x> is float, and we try:
+y+1 // we get 1004, becouse the next block of <x> starts with 1024, since <x> in a float.
+
+// addition formula:
+Pointer+n = Pointer+sizeof(type of pointer)*n
+
+// examples:
+// we assume address in each example is 1000
+int i=5, *ii;
+float f=5.8, *ff;
+char c='u', **c;
+
+ii+3 = 1006 // <int> counsume 2 bytes
+ff+3 = 1012 // <float> counsume 4 bytes
+cc+3 = 1003 // <char counsume 1 byte
+
+// subtraction int pointers same as addition
+----------------------------------------------
+// we can subtract 2 pointers if those pointers have same type.
+// example:
+int i=5, z=4, *ii, *zz, a;
+ii = &i;
+zz = &z;
+// we assume: <i> address is 1000, and <z> address is 1020
+
+// now we need subtract these pointers, we can do it, becouse these pointers have same type <int>, so:
+a = zz - ii;
+// we get <10> becouse 1020-1000=20 and these pointers are int, and int consume 2 bytes so we devide our Anser <20> by <2> and get final Answer <10>, this Answer tell us how many blocks we have can contain <int> value
+
+// formula
+Pointer1 - Pointer2 = Literal subtraction / sizeof(type of pointer)
+----------------------------------------------
+// swap values using function and pointers
+// remember we can't return +1 values from function
+void swap(int*, int*);
+main()
+{
+	int a,b;
+	printf("Enter two numbers");
+	scanf("%d%d", &a, &b);
+	swap(a,b);
+	printf("a=%d b=%d",a,b);
+}
+void swap(int x, int y)
+{
+	int t;
+	t = x;
+	x = y;
+	y = t;
+}
+// obeve we created a fucntion and successfuly swaped the values, but we need this values as a <a> and <b> to print theese values swaped, but remember functions in C cannot return more than one value, here comes pointers in actions, so:
+void swap(int*, int*);
+main()
+{
+	int a,b;
+	printf("Enter two numbers");
+	scanf("%d%d", &a, &b);
+	swap(&a,&b);
+	printf("a=%d b=%d",a,b);
+}
+void swap(int *x, int *y)
+{
+	int t;
+	t = *x;
+	*x = *y;
+	*y = t;
+}
+// in second function we swaped these addresses, same as <scanf> function, we use like:
+// <scanf("%d%d", &d, &f)> yahan ham address dy rahy hen bajay variable name k, q k agar variable names dy bhi jaen to wo return ho kar <d> or <f> me kesy assign ho saken gy?? isi lye ham scanf ko address dety hen or vo us address me ja kar us me user input assign kar deta h  
+----------------------------------------------
+// string
+string k functions me ham zyada tar address hi paas kar rahy hoty hen, eg: 
+c har s[4]="amir"
+printf(s) // <s> here address of first block (which is here char] in "amir". suppose address of "a" in "amir" is 1000, so whenever you type <s> its treated as 1000, <printf(s)> == <printf(&s[0])>
+----------------------------------------------
+// Structure
+// Structure is way to group variables
+// Structure is a collection of dissimilar elements
+// Defining strucure means creating new data type
+// kahin bhi create kya ja sakta h, or is ka access bhi wahi tak ho ga jahan define kya gya h
+
+// syntax
+struct your_datatype_name
+{
+	// member variable declarations
+};
+
+
+// create a variable of user defined type:
+// 1- :
+struct your_datatype_name
+{};
+struct your_datatype_name variable_name;
+
+// 2-:
+struct your_datatype_name
+{}variable1,variable2,variable3;
+
+// example:
+struct date
+{
+	int d,y;
+	char m[12];
+}
+void main()
+{
+	struct date today, d1, d2; // <today> and <d1>  and <d2> all are variables of type <date>
+	today.d = 17;
+	today.m = "Decembter";
+	today.y = 2018;
+	prinf("today:\n%d%s%d\n", &today.d, &today.m, &today.y);
+
+	d1 = today;
+	printf("d1\n%d%s%d\n",&d1.d, &d1.m, d1.y);
+
+	printf("Enter today's date");
+	scanf("%d%s%d", &d2.d, d2.m, d2.y);
+	printf("\nd2%d%s%d", &d2.d, &d2.m, &d2.y);
+
+}
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
 
 ----------------------------------------------
 
@@ -380,5 +591,21 @@ main()
 ----------------------------------------------
 
 ----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+----------------------------------------------
+
+
+
+
+
+
+
 
-v
