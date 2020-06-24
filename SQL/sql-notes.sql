3eba2959eb7e929c7b1469873f751e13e49cb18f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-25 00:17:37 +0500

    updated

---
+++ SQL/sql-notes.sql
@@ -0,0 +1,1 @@
+baz dafa ham kafa 1 sath insert kar rahy hoty hen, to data insert nahi ho pata, 1 wajah ye ho sakti h k aap k for-insert data ka size above limit h, limit ko barhany k lye ye karen: SET GLOBAL max_allowed_packet=1073741824;, is me ham 1GB tak increase kar rahy hen.
