10ccdb291cd93a6a904f8ea1def8aacc28a93ac1
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 11:16:32 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -1,6 +1,7 @@
 
-check docker version ................ sudo docker version
-docker hub .......... hub.docker.com
+docker version ........... check docker version
+# docker hub .......... hub.docker.com
 print hello-world on screen ................ docker run docker/whalesay cowsay Hello-world
-docker run ..............run docker from an image on the docker host if the image is exist in local, if image is not present on the host it will go out to docker hub and pulled image down, but this is only done the first time. for the subsequant executaion the same image will be reused.
+docker run .............. run docker from an image on the docker host if the image is exist in local, if image is not present on the host it will go out to docker hub and pulled image down, but this is only done the first time. for the subsequant executaion the same image will be reused.
+docker ps  .............. list all running containers, and some basic information about them.
 
