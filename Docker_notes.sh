147a6311cf6fc94cca99195e05d8dbb5da94d7c4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 13:40:08 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -4,7 +4,7 @@ docker version ........... check docker version
 print hello-world on screen ........... docker run docker/whalesay cowsay Hello-world
 docker run .............. run docker from an image on the docker host if the image is exist in local, if image is not present on the host it will go out to docker hub and pulled image down, but this is only done the first time. for the subsequant executaion the same image will be reused.
 docker ps  .............. list all running containers, and some basic information about them.
-docker ps -a ...........  to see all containers running or not, all running as well as previously stoped or exited containers.
+docker ps -a ...........  to see all containers running or not, all running as well as previously stoped or exited containers. Note: not list deleted containers.
 docker stop <container_ID|container_Name> ............ to stop a container.
 docker rm <container_Name> ............ to remove a stoped/exited container permenantily. Note: remove multiple containers in one command ............. docker rm cont1_Name cont2_Name 
 docker rmi <container_Name> ........... to remove a stoped/exited container permenantily. Note: you must ensure that no containers are running of that image before attempting to remove the image. you must stop and delet all dependent containers to be able to delete an image.
