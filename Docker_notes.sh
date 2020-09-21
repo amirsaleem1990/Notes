7b2d645f02e9bfb9556ba18d3eabdf28b4f1cf43
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 11:19:50 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -5,4 +5,5 @@ print hello-world on screen ................ docker run docker/whalesay cowsay H
 docker run .............. run docker from an image on the docker host if the image is exist in local, if image is not present on the host it will go out to docker hub and pulled image down, but this is only done the first time. for the subsequant executaion the same image will be reused.
 docker ps  .............. list all running containers, and some basic information about them.
 docker ps -a ...........  to see all containers running or not, all running as well as previously stoped containers.
+docker stop <container_ID|container_Name> ............ to stop a container.
 
