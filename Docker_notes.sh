3ddc7000ef7eb0b67fd64b4692299972caec86fb
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 13:39:00 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -6,7 +6,7 @@ docker run .............. run docker from an image on the docker host if the ima
 docker ps  .............. list all running containers, and some basic information about them.
 docker ps -a ...........  to see all containers running or not, all running as well as previously stoped or exited containers.
 docker stop <container_ID|container_Name> ............ to stop a container.
-docker rm <container_Name> ............ to remove a stoped/exited container permenantily.
+docker rm <container_Name> ............ to remove a stoped/exited container permenantily. Note: remove multiple containers in one command ............. docker rm cont1_Name cont2_Name 
 docker rmi <container_Name> ........... to remove a stoped/exited container permenantily. Note: you must ensure that no containers are running of that image before attempting to remove the image. you must stop and delet all dependent containers to be able to delete an image.
 docker images ......................... list all images present on our host.
 docker pull image_Name  ............... download an image
