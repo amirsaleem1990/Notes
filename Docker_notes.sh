5096bbd67970fb7da632d7e4d7b6c0de570fda19
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 11:12:12 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -2,5 +2,6 @@
 check docker version ................ sudo docker version
 docker hub .......... hub.docker.com
 print hello-world on screen ................ docker run docker/whalesay cowsay Hello-world
+run docker from an image ............ docker run
 
 
