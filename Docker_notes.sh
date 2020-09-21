3618718d688969c3bc32a10f78a51d6e0fe172b6
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 13:22:30 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -17,5 +17,5 @@ dockder exec container_Name command ... executing a command on a running contain
 
 # Run - attach and detach
 docker run image_name ................. runs in the foreground (attachded mode), you will be attaced to the console or the standard out of the docker container and you will see the output of the application on your screen. you won't be able to do anything else in this console until this docker container stops. 
-
+docker run -d image_name .............. runs in the backgroud  (detached  mode), 
 
