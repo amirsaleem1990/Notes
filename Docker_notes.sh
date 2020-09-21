ec3e458aa6ddc902343220608f0b77f77e4a4b12
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-21 11:18:41 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -4,4 +4,5 @@ docker version ........... check docker version
 print hello-world on screen ................ docker run docker/whalesay cowsay Hello-world
 docker run .............. run docker from an image on the docker host if the image is exist in local, if image is not present on the host it will go out to docker hub and pulled image down, but this is only done the first time. for the subsequant executaion the same image will be reused.
 docker ps  .............. list all running containers, and some basic information about them.
+docker ps -a ...........  to see all containers running or not, all running as well as previously stoped containers.
 
