57fa511cc6c58e60d3f33d28bcd0f6e178add4a4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-22 17:42:56 +0500

    updated

--- Docker_notes.sh
+++ Docker_notes.sh
@@ -144,4 +144,4 @@ networks:
 	> docker -H=10.123.2.1:2375 run nginx
 #-----------------
 
-# by default there is no restriction as to how mush of a resource a container can use and hence a cantainer may end up utilize all of the resource on the underlying host.
+# by default there is no restriction as to how mush of a resource a container can use and hence a cantainer may end up utilize all of the resource on the underlying host. but there is a way to restrice the amount of CPU or Memory a cantainer can use. 
